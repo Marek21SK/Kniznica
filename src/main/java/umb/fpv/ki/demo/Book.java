5049b0b11f5e8da2cb7a1f4e3a1df32e365a4d5c
mirka <mirka.syc@gmail.com>
2022-02-22 19:11:12 +0100

    Book

--- src/main/java/umb/fpv/ki/demo/Book.java
+++ src/main/java/umb/fpv/ki/demo/Book.java
@@ -1,7 +1,49 @@
 package umb.fpv.ki.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
-    public class Book{
+@Entity
+public class Book {
+    @Id @GeneratedValue(strategy = javax.persistence.GenerationType.AUTO)
+    private long bookId;
 
-        public String name;
+    @NotNull
+    private String authorName;
+    @NotNull
+    private String title;
+    @NotNull
+    private short publishedYear;
+    @NotNull
+    private int stock;
+
+    public long getBookId() {
+        return bookId;
     }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public short getPublishedYear() {
+        return publishedYear;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void borrowedOne() {
+        this.stock--;
+    }
+
+    public void returnedOne() {
+        this.stock++;
+    }
+}
---
+++ src/main/java/umb/fpv/ki/demo/interf.java
@@ -0,0 +1,14 @@
+package umb.fpv.ki.demo;
+
+
+package com.example.sbl.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.sbl.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
+    List<Book> findByAuthorName(String authorName);
+}
