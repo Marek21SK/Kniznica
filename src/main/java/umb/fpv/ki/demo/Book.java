9baa4afde35d370c858d21e97bec79ce55a27a8f
mirka <mirka.syc@gmail.com>
2022-02-24 19:23:01 +0100

    Book

---
+++ src/main/java/umb/fpv/ki/demo/Book.java
@@ -0,0 +1,34 @@
+package umb.fpv.ki.demo;
+
+public class Book {
+    private String isbn;
+    private String autfirstName;
+    private String autlastName;
+    private String title;
+    private String count;
+
+    public String getIsbn() {return isbn;}
+
+    public void setIsbn(String isbn) {this.isbn = isbn; }
+
+    public String getAutfirstName() {return autfirstName;}
+
+    public void setAutfirstName(String isbn) {this.autfirstName = autfirstName; }
+
+    public String getAutlastName() {return autlastName;}
+
+    public void setAutlastName(String autlastName) {this.autlastName = autlastName; }
+
+    public String getTitle() {return title;}
+
+    public void setTitle(String title) {this.title = title; }
+
+    public String getCount() {return count;}
+
+    public void setCount(String count) {this.count = count; }
+
+
+
+
+
+}
--- src/main/java/umb/fpv/ki/demo/BookController.java
+++ src/main/java/umb/fpv/ki/demo/BookController.java
@@ -2,42 +2,102 @@
 package umb.fpv.ki.demo;
 
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import javax.validation.Valid;
+public class BookController<autfirstName> {
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+    List<Book> books;
 
-import com.example.sbl.model.Book;
-import com.example.sbl.repository.BookRepository;
-
-@RestController
-@RequestMapping("book")
-public class BookController {
-    @Autowired
-    BookRepository bookRepository;
-
-    @GetMapping
-    public List<Book> getAllBooks(){
-        return bookRepository.findAll();
+    public BookController() {
+        this.books = init();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Book>>getBookById(@PathVariable(value = "id") long bookId) {
-        Optional<Book> book = bookRepository.findById(bookId);
-        return ResponseEntity.ok().body(book);
+    public List<Book> init() {
+        List<Book> books = new ArrayList<>();
+
+        Book Book_1 = new Book();
+        Book_1.setIsbn("97880755442703");
+        Book_1.setAutfirstName("Kiera");
+        Book_1.setAutlastName("Cassova");
+        Book_1.setTitle("Koruna");
+        Book_1.setCount("10");
+
+        Book Book_2 = new Book();
+        Book_2.setIsbn("978809972040");
+        Book_2.setAutfirstName("Baja");
+        Book_2.setAutlastName("Dolce");
+        Book_2.setTitle("Habibi");
+        Book_2.setCount("15");
+
+        Book Book_3 = new Book();
+        Book_3.setIsbn("9788022206846");
+        Book_3.setAutfirstName("Rick");
+        Book_3.setAutlastName("Yancey");
+        Book_3.setTitle("Piata clna");
+        Book_2.setCount("8");
+
+        return books;
+
     }
 
-    @PostMapping
-    public Book addBook(@Valid @RequestBody Book book) {
-        return bookRepository.save(book);
+    @GetMapping("/api/books")
+    public List<Book> getBooks(@RequestParam(required = false) String autfirstName) {
+
+
+    if(autfirstName ==null)
+
+    {
+        return this.books;
     }
+
+    List<Book> f_Books = new ArrayList<>();
+    for(Book book:books){
+
+        String bookautfirstName = null;
+        if (book.getAutfirstName().equals(bookautfirstName)) {
+            f_Books.add(book);
+        }
+    }
+    return f_Books;
+
 }
+
+@GetMapping("/api/bookIsbn")
+    public List<Book> getBooksIsbn(@RequestParam(required = false)String bookIsbn){
+        if(bookIsbn == null){
+            return this.books;
+        }
+        List<Book>f_Books = new ArrayList<>();
+    for(Book book:books) {
+        if (book.getIsbn().equals(bookIsbn)) {
+            f_Books.add(book);
+        }
+    }
+    return f_Books;
+
+    }
+
+    @PostMapping("/api/book")
+    public List<Book> create_Book(@RequestBody Book book){
+        this.books.add(book);
+        return books;
+    }
+    @DeleteMapping("/api/book")
+    public void delete_Book(@PathVariable Integer bookIsbn) {this.books.remove(this.books);}
+
+    @PutMapping("api/book{bookIsbn}")
+    public List<Book> put_Book(@PathVariable Integer bookIsbn, @RequestBody Book book){
+        this.books.get(bookIsbn).setCount(book.getCount());
+        this.books.get(bookIsbn).setAutfirstName(book.getAutfirstName());
+        this.books.get(bookIsbn).setAutlastName(book.getAutlastName());
+        this.books.get(bookIsbn).setTitle(book.getTitle());
+        this.books.get(bookIsbn).setIsbn(book.getIsbn());
+        return books;
+    }
+    }
+
+
