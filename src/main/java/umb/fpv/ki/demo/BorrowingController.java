aa4dbc641a79f7587d01c45d9828305f30e7f7a0
davidHajdu <davidhajdu25@gmail.com>
2022-02-28 21:40:39 +0100

    BorrowingService + BorrowingController

--- src/main/java/umb/fpv/ki/demo/BorrowingController.java
+++ src/main/java/umb/fpv/ki/demo/BorrowingController.java
@@ -2,61 +2,33 @@ package umb.fpv.ki.demo;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-
 
 @RestController
 public class BorrowingController{
-    List<Borrowing> borrowings;
+    private List<Borrowing> borrowing;
 
-    public BorrowingController(){
-        this.borrowings = init_1();
-    }
-
-    public List<Borrowing> init_1(){
-        List<Borrowing> borrow = new ArrayList<>();
-
-        Borrowing borrowing1 = new Borrowing();
-        borrowing1.setBorrowingId("1");
-        borrow.add(borrowing1);
-
-        Borrowing borrowing2 = new Borrowing();
-        borrowing2.setBorrowingId("2");
-        borrow.add(borrowing2);
-
-        return borrow;
+    private BorrowingService borrowingService;
+    public BorrowingController(BorrowingService borrowingService){
+        this.borrowingService = borrowingService;
     }
 
 
     @GetMapping("/api/borrowings")
     public List<Borrowing> getBorrowing(@RequestParam(required = false) String borrowingId){
-        if(borrowingId == null){
-            return this.borrowings;
-        }
-
-        List<Borrowing> f_Borrowings = new ArrayList<>();
-
-        for (Borrowing borrowings : borrowings){
-            if(borrowings.getBorrowingId().equals(borrowingId)){
-                f_Borrowings.add(borrowings);
-            }
-        }
-
-        return f_Borrowings;
+        return this.borrowingService.getBorrowings(borrowingId);
     }
 
 
     @PostMapping("api/borrowings")
-    public  List<Borrowing> create_Borrowing(@RequestBody Borrowing borrowing){
-        this.borrowings.add(borrowing);
+    public  List<Borrowing> createBorrowing(@RequestBody Borrowing borrowing){
+        return borrowingService.createBorrowing(borrowing);
 
-        return borrowings;
     }
 
 
     @DeleteMapping("/api/borrowings/{borrowingId}")
     public void delete_Borrowing(@PathVariable Integer borrowingId){
-        this.borrowings.remove(this.borrowings.get(borrowingId));
+        this.borrowingService.deleteBorrowing(borrowingId);
     }
 }
