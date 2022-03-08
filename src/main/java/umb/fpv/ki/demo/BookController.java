624203acca5286f9acbf64fda93e1246a6bece97
mirka <mirka.syc@gmail.com>
2022-03-08 17:45:00 +0100

    Book

--- src/main/java/umb/fpv/ki/demo/BookController.java
+++ src/main/java/umb/fpv/ki/demo/BookController.java
@@ -8,39 +8,34 @@ import java.util.List;
 
 @RestController
 public class BookController {
-
-    private List<Book> books;
-
-
     private BookService bookService;
 
-    public BookController(BookService bookService){
+    public BookController(BookService bookService) {
         this.bookService = bookService;
     }
 
     @GetMapping("/api/books")
-    public List<Book> getBooks(@RequestParam(required = false) String bookAuthor) {
+    public List<BookDto> getBooks(@RequestParam(required = false) String bookAuthor) {
         return bookService.getBooks(bookAuthor);
     }
 
-    @DeleteMapping("/api/books/{bookId}")
-    public void deleteBook(@PathVariable Integer bookId){
-        bookService.deleteBook(bookId);
-    }
-
-    @PutMapping("/api/books/{bookId}")
-    public void updateBook(@PathVariable Integer bookId, @RequestBody Book book){
-        bookService.updateBook(bookId, book);
-    }
-
     @GetMapping("/api/books/{bookId}")
-    public Book getBook(@PathVariable Integer bookId){
+    public BookDto getBook(@PathVariable Long bookId) {
         return bookService.getBook(bookId);
     }
 
     @PostMapping("/api/books")
-    public Integer createBook(@RequestBody Book book){
-        return bookService.createBook(book);
+    public Long createBook(@RequestBody BookDto bookDto) {
+        return bookService.createBook(bookDto);
     }
 
+    @DeleteMapping("/api/books/{bookId}")
+    public void deleteBook(@PathVariable int bookId) {
+        bookService.deleteBook(bookId);
+    }
+
+    @PutMapping("/api/books/{bookId}")
+    public void updateBook(@PathVariable int bookId, @RequestBody BookDto bookDto) {
+        bookService.updateBook(bookId, bookDto);
+    }
 }
