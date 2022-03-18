d59e727c87f034bfff2a73a9cdc645d93f84b644
Marek Pástor <marekpastorml@gmail.com>
2022-03-18 14:47:08 +0100

    Cvičenie 16.3.2022

--- src/main/java/umb/fpv/ki/demo/UserController.java
+++ src/main/java/umb/fpv/ki/demo/UserController.java
@@ -12,25 +12,25 @@ public class UserController{
         this.userService = userService;
     }
 
-    @GetMapping("/api/users")
+    @GetMapping("/api/customers")
     public List<UserDto> getUsers(@RequestParam(required = false) String userFirstName){
         return userService.getUsers(userFirstName);}
 
-    @GetMapping("/api/userId")
+    @GetMapping("/api/customerId")
     public List<UserDto> getUsersId(@RequestParam(required = false) String userID){
         return userService.getUserId(userID);}
 
-    @PostMapping("/api/users")
+    @PostMapping("/api/customers")
     public String create_User(@RequestBody UserDto user){
       return userService.create_User(user);
     }
 
-    @DeleteMapping("api/users/{userId}")
+    @DeleteMapping("api/customers/{customerId}")
     public void delete_User(@PathVariable int userId) {
         userService.delete_User(userId);
     }
 
-    @PutMapping("api/users{userId}")
+    @PutMapping("api/customers{customerId}")
     public void put_User(@PathVariable int userId, @RequestBody UserDto user){
         userService.put_User(userId, user);
     }
